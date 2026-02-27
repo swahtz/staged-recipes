9f07418553e4c49496b4e2dda4f8c59dcca044b2
Jonathan Swartz <2375296+swahtz@users.noreply.github.com>
2026-02-28 09:52:24 +1300

    Remove run_test.py from nanovdb-editor recipe
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- recipes/nanovdb-editor/run_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-try:
-    import nanovdb_editor
-except ImportError:
-    print("Failed to import nanovdb_editor. Exiting without running tests.")
-    sys.exit(0)
-
-print("nanovdb_editor imported successfully")
